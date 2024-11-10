80f19a4461e8eab60d0634cea82fb4e481677b3c
GABY.VT <JOSETH.VALVERDETREJOS@ucr.ac.cr>
2024-11-09 20:44:59 -0600

    supervisor back

--- datos/src/app/routes.php
+++ datos/src/app/routes.php
@@ -7,6 +7,7 @@ use Slim\Routing\RouteCollectorProxy;
 // require __DIR__ . '/../controllers/Cliente.php'
 // Recurso Cliente
 $app->group('/api',function(RouteCollectorProxy $api){
+
     $api->group('/cliente',function(RouteCollectorProxy $cliente){
         $cliente->post('', Cliente::class . ':create'); //Crea un cliente
         $cliente->get('/read[/{id}]', Cliente::class . ':read'); //Consulta todos los clientes
@@ -37,6 +38,7 @@ $app->group('/api',function(RouteCollectorProxy $api){
         $usr->patch('/change/passw/{idUsuario}', Usuario::class . ':cambiarPassw');
         $usr->patch('/change/rol/{idUsuario}', Usuario::class . ':cambiarRol');
     });
+
     $api->group('/supervisor', function (RouteCollectorProxy $supervisor) {
         $supervisor->post('', supervisor::class . ':create');
         $supervisor->get('/read[/{id}]', supervisor::class . ':read');
@@ -44,41 +46,11 @@ $app->group('/api',function(RouteCollectorProxy $api){
         $supervisor->put('/{id}', supervisor::class . ':update');
         $supervisor->delete('/{id}', supervisor::class . ':delete');
     });
+
     $api->group('/Products', function (RouteCollectorProxy $Products) {
         $Products->get('/read[/{id}]', Panes::class . ':read');
     });
     
-    $api->group('/orders', function (RouteCollectorProxy $orders) {
-        $orders->post('', orders::class . ':create');
-        $orders->get('/read[/{id}]', orders::class . ':read');
-        $orders->get('/filtro', orders::class . ':filtrar');
-        $orders->put('/{id}', orders::class . ':update');
-        $orders->delete('/{id}', orders::class . ':delete');
-    });
-    
-    $api->group('/order_items', function (RouteCollectorProxy $order_items) {
-        $order_items->post('', order_items::class . ':create');
-        $order_items->get('/read[/{id}]', order_items::class . ':read');
-        $order_items->get('/filtro', order_items::class . ':filtrar');
-        $order_items->put('/{id}', order_items::class . ':update');
-        $order_items->delete('/{id}', order_items::class . ':delete');
-    });
-    
-    $api->group('/carts', function (RouteCollectorProxy $carts) {
-        $carts->post('', carts::class . ':create');
-        $carts->get('/read[/{id}]', carts::class . ':read');
-        $carts->get('/filtro', carts::class . ':filtrar');
-        $carts->put('/{id}', carts::class . ':update');
-        $carts->delete('/{id}', carts::class . ':delete');
-    });
-    
-    $api->group('/cart_items', function (RouteCollectorProxy $cart_items) {
-        $cart_items->post('', cart_items::class . ':create');
-        $cart_items->get('/read[/{id}]', cart_items::class . ':read');
-        $cart_items->get('/filtro', cart_items::class . ':filtrar');
-        $cart_items->put('/{id}', cart_items::class . ':update');
-        $cart_items->delete('/{id}', cart_items::class . ':delete');
-    });
 });
 
    
