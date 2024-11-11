773c90568bcad578fe1b905875ab0bd3bec92067
GABY.VT <JOSETH.VALVERDETREJOS@ucr.ac.cr>
2024-11-11 17:35:20 -0600

    Front-End

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,IndexComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'presentacion';
+}
+
+
---
+++ front/src/app/components/index/index.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent {
+  images = [
+    'assets/images/image1.jpg',
+    'assets/images/image2.jpg',
+    'assets/images/image3.jpg',
+    'assets/images/image4.jpg'
+  ];
+  currentIndex = 0;
+
+  get translateX() {
+    return -this.currentIndex * 100;
+  }
+
+  next() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1;
+    }
+  }
+}
