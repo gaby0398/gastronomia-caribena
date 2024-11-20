85b584e4f49297f58c728de3ede209ebd54fe149
Buterflyyy <alpizarzunigaalison@gmail.com>
2024-11-20 14:09:26 -0600

    Cambios header

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { GestionUsuariosComponent } from './components/gestion-usuarios/gestion-usuarios.component';
+import { InformacionComponent } from './components/informacion/informacion.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
-    { path: 'gestion-usuarios', component: GestionUsuariosComponent }
+    { path: 'gestion-usuarios', component: GestionUsuariosComponent },
+    { path: 'informacion', component: InformacionComponent },
+    { path: 'login', component: LoginComponent }
       // Ruta por defecto que apunta a IndexComponent
 ];
