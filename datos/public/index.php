a397b2326982bb9e5092e0e96757048acec81cd8
JosueMS1408 <josue.moralessuarez@ucr.ac.cr>
2024-11-27 23:51:15 -0600

    Evitar que salgan advertencias en las solicitudes Http

--- datos/public/index.php
+++ datos/public/index.php
@@ -3,7 +3,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-API-KEY, Origen, X-Request-Width, Content-Type, Accept, Access-Control-Request-Method, Authorization');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH');
 header('Allow: GET, POST, OPTIONS, PUT, DELETE, PATCH');
- 
+
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 
 if ($_SERVER['REMOTE_ADDR'] != "192.168.0.13") {
     die('Petición no válida');
--- negocio/public/index.php
+++ negocio/public/index.php
@@ -4,6 +4,10 @@ header('Access-Control-Allow-Headers: X-API-KEY, Origen, X-Request-Width, Conten
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH');
 header('Allow: GET, POST, OPTIONS, PUT, DELETE, PATCH');
 
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     die('Metodo no permitido');
 }
