23ad910698ac59edbd672654b4707ddf610ffe4a
GABY.VT <JOSETH.VALVERDETREJOS@ucr.ac.cr>
2024-11-29 09:30:47 -0600

    se agrega el Proyecto

--- front/src/app/components/boton-gestion-cerrar/boton-gestion-cerrar.component.ts
+++ front/src/app/components/boton-gestion-cerrar/boton-gestion-cerrar.component.ts
@@ -1,37 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-boton-gestion-cerrar',
   standalone: true,
-  imports: [CommonModule, MatIconModule, RouterModule], 
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './boton-gestion-cerrar.component.html',
   styleUrls: ['./boton-gestion-cerrar.component.css'],
 })
 export class BotonGestionCerrarComponent {
   isOpen = false;
 
- // Abre o cierra el menú al hacer clic en el botón
- toggleMenu() {
-  this.isOpen = !this.isOpen;
- }
-  // Escucha clics globales en el documento
-@HostListener('document:click', ['$event'])
-onClickOutside(event: Event) {
-  const targetElement = event.target as HTMLElement;
+  constructor(private authService: AuthService, private router: Router) {}
 
-  // Verifica si el clic ocurrió fuera del menú
-  if (!targetElement.closest('.hamburger-container')) {
-    this.isOpen = false; // Cierra el menú si está abierto
+  // Abre o cierra el menú al hacer clic en el botón
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  // Escucha clics globales en el documento
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const targetElement = event.target as HTMLElement;
+
+    // Verifica si el clic ocurrió fuera del menú
+    if (!targetElement.closest('.hamburger-container')) {
+      this.isOpen = false; // Cierra el menú si está abierto
+    }
+  }
+
+  // Maneja el clic en el botón de cerrar sesión
+  cerrarSesion() {
+    this.authService.logout();
+    this.router.navigate(['/login']); // Redirigir a la página de inicio de sesión
   }
 }
-
-
-}
-
-function onClickOutside(event: Event | undefined, Event: { new(type: string, eventInitDict?: EventInit): Event; prototype: Event; readonly NONE: 0; readonly CAPTURING_PHASE: 1; readonly AT_TARGET: 2; readonly BUBBLING_PHASE: 3; }) {
-  throw new Error('Function not implemented.');
-}
-
